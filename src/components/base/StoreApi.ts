3b8cbb7964c74bcad078352a869f9e4fa0403cb1
Евгения Озерина <e.ozerina@vk.team>
2025-12-15 19:45:27 +0300

    prettier

--- src/components/base/StoreApi.ts
+++ src/components/base/StoreApi.ts
@@ -1,15 +1,17 @@
 import { IApi, IOrderResponse, IOrder, IProduct } from "../../types";
 
 export class StoreApi {
-    constructor(private api: IApi) {
-        this.api = api; 
-    }
+  constructor(private api: IApi) {
+    this.api = api;
+  }
 
-    async getProducts(): Promise<IProduct[]> {
-        const response = await this.api.get<{ total: number; items: IProduct[] }>('/product/');
-        return response.items;
-    }
-    createOrder(data: IOrder): Promise<IOrderResponse> {
-        return this.api.post('/order/', data);
-    }
-    }
+  async getProducts(): Promise<IProduct[]> {
+    const response = await this.api.get<{ total: number; items: IProduct[] }>(
+      "/product/"
+    );
+    return response.items;
+  }
+  createOrder(data: IOrder): Promise<IOrderResponse> {
+    return this.api.post("/order/", data);
+  }
+}
